1b79f44143acb91f86dabf11bf0215d94798e020
dsukanta <sukanta@semicolonites.com>
2023-10-10 12:09:48 +0530

    basic structure ready

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -1,36 +1,62 @@
-import { Button, Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Button} from '@rneui/base';
+import {useNavigation} from '@react-navigation/native';
+
 
 const CustomHeader = () => {
+
+  const navigation= useNavigation();
+
   return (
     <View style={styles.container}>
       <View>
-        <Image source={require('../../assests/logo.jpg')} style={styles.logo}/>
+        <Image source={require('../../assests/logo.jpg')} style={styles.logo} />
       </View>
       <View style={styles.btnContainer}>
-        <Button title='Login'/>
-        <Button title='Signup'/>
+        <Button
+          title="Login"
+          buttonStyle={{
+            backgroundColor: 'rgba(78, 116, 289, 1)',
+            borderRadius: 5,
+          }}
+          containerStyle={{
+            width: 'auto',
+          }}
+          titleStyle={{color: 'white', marginHorizontal: 10}}
+          onPress={()=>navigation.navigate('Login')}
+        />
+        <Button
+          title="SignUp"
+          buttonStyle={{backgroundColor: 'rgba(39, 39, 39, 1)', borderRadius: 5,}}
+          containerStyle={{
+            width: 'auto',
+          }}
+          titleStyle={{color: 'white', marginHorizontal: 10}}
+          onPress={()=>navigation.navigate('Signup')}
+        />
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default CustomHeader
+export default CustomHeader;
 
 const styles = StyleSheet.create({
-    container:{
-        flexDirection:'row',
-        padding:5,
-        justifyContent:'space-between'
-    },
-    logo:{
-        width:50,
-        height:50,
-        borderRadius:50,
-        resizeMode:'contain',
-    },
-    btnContainer:{
-        flexDirection:'row',
-        gap:10,
-    }
-})
+  container: {
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'space-between',
+    alignItems:'center'
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    resizeMode: 'contain',
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
--- src/navigator/AppNavigator.js
+++ src/navigator/AppNavigator.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import Splash from '../screens/Splash';
 import Main from '../screens/Main';
 import TabNavigator from './TabNavigator';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
 
 const Stack= createStackNavigator();
 
@@ -14,6 +16,8 @@ const AppNavigator = () => {
     <Stack.Navigator>
         <Stack.Screen name='Splash' component={Splash} options={{headerShown:false}}/>
         <Stack.Screen name='Main' component={TabNavigator} options={{headerShown:false}}/>
+        <Stack.Screen name='Login' component={Login} options={{headerShown:false}}/>
+        <Stack.Screen name='Signup' component={Signup} options={{headerShown:false}}/>
     </Stack.Navigator>
    </NavigationContainer>
   )
